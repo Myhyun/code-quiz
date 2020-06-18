1751364d6fd7b06d0b3254450b82645a7e7a793c
Myhyun <mattyihyun@gmail.com>
2020-06-17 18:36:56 -0700

    fixed code to display all questions

--- assets/quizScript.js
+++ assets/quizScript.js
@@ -1,53 +1,53 @@
 var myQuestions = [
     {
         question: "What does HTML stand for?",
-        A: "Hyper Text Markup Language",
-        B: "Human Text Master Language",
-        C: "Hastey Typeup Mark Language",
+        a: "Hyper Text Markup Language",
+        b: "Human Text Master Language",
+        c: "Hastey Typeup Mark Language",
         answer: "A"
     },
 
     {
         question: "What HTML tag must we use in order to write JavaScript Code?",
-        A: "<js>",
-        B: "<javascript",
-        C: "<script>",
+        a: "js",
+        b: "javascript",
+        c: "script",
         answer: "C"
     },
     {
         question: "Which of the following uses correct syntax when referring to an external fun.js script?",
-        A: "<script src=”fun.js”>",
-        B: "<script href=”fun.js”>",
-        C: "<script ref=”fun.js”>",
+        a: "script src=”fun.js”",
+        b: "script href=”fun.js”",
+        c: "script ref=”fun.js”",
         answer: "A"
 
     },
     {
         question: "Which of the following is used to call a function in JavaScript?",
-        A: "call function()",
-        B: "function()",
-        C: "cfunction function()",
+        a: "call function()",
+        b: "function()",
+        c: "cfunction function()",
         answer: "B"
     },
     {
         question: "Which of the following is not one of the three main web development languages?",
-        A: "HTML",
-        B: "CSS",
-        C: "Angular",
+        a: "HTML",
+        b: "CSS",
+        c: "Angular",
         answer: "C"
     },
     {
         question: "Which if the following is used to open a message screen when a page is loaded?",
-        A: "message()",
-        B: "alert()",
-        C: "ping()",
+        a: "message()",
+        b: "alert()",
+        c: "ping()",
         answer: "B"
     }
 
 ];
 
 
-
+var question = 0;
 var userPos = 0;
 var userAns = 0;
 var possibleAns = 0;
@@ -57,14 +57,15 @@ var ansC = 0;
 var quiz = 0;
 var userScore = 0
 
-
-
 var timeElement = document.querySelector("#time");
 var secondsLeft = 120;
 
+//call the setTime and startQuiz function
 
 setTime();
 startQuiz();
+
+//Function to start the timer and have it countdown from 2 minutes, if it hits 0 calls the showEndScreen function
 
 function setTime() {
     var timerInterval = setInterval(function () {
@@ -74,56 +75,58 @@ function setTime() {
             clearInterval(timerInterval);
             showEndScreen();
         }
-        
+
 
     }, 1000);
 }
 
+//Function to start the quiz and display the first question once page loads
+
 function startQuiz() {
-    quiz = document.getElementById("quizQuestions");
-    if(userPos >= myQuestions.length) {
+    var quiz = document.getElementById("quizQuestions");
+    if (userPos >= myQuestions.length) {
         showEndScreen();
     }
 
-    document.getElementById("quizPos").innerHTML = "Question "+(userPos+1)+" of "+myQuestions.length;
+    
+    document.getElementById("quizPos").innerHTML = "Question " + (userPos + 1) + " of " + myQuestions.length;
 
     question = myQuestions[userPos].question;
-    ansA = myQuestions[userPos].A;
-    ansB = myQuestions[userPos].B;
-    ansC = myQuestions[userPos].C;
+    var ansA = myQuestions[userPos].a;
+    var ansB = myQuestions[userPos].b;
+    var ansC = myQuestions[userPos].c;
 
-    quiz.innerHTML += "<h3>" +question+ "</h3>";
+    quiz.innerHTML += "<h3>" + question + "</h3>";
     quiz.innerHTML += "<label> <input type ='radio' name='possibleAns' value='A'> " + ansA + "</label><br>";
     quiz.innerHTML += "<label> <input type ='radio' name='possibleAns' value='B'> " + ansB + "</label><br>";
     quiz.innerHTML += "<label> <input type ='radio' name='possibleAns' value='C'> " + ansC + "</label><br>";
     quiz.innerHTML += "<button onclick='checkAns()'>Submit Answer</button>";
-
+    
 }
+
+//Function to check the answer that the user inputs, if correct adds +1 to the user's score and updates the user's position to correctly display what question # they are at
 
 function checkAns() {
     possibleAns = document.getElementsByName("possibleAns");
-    for (var i = 0; i< possibleAns.length; i++) {
-        if(possibleAns[i].checked) {
-            userAns = possibleAns[i].value;
+    for (var i = 0; i < possibleAns.length; i++) {
+        if (possibleAns[i].checked) {
+            var userAns = possibleAns[i].value;
         }
     }
 
     if (userAns == myQuestions[userPos].answer) {
         userScore++;
-    }
-    userPos++;
 
+    } else {
+        secondsLeft = secondsLeft - 10;
+    };
+
+    userPos++;
+    
     startQuiz();
 }
 
-
-
-
-
-
-
-
-
+//Function that brings up the end screen that will hold their score and allow them to enter initials
 
 function showEndScreen() {
     timeElement.textContent = " ";
